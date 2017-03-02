021eb6328854329f1a47b6585505d643fcf24d2a
Charlie Ciccia <cciccia@gmail.com>
2017-03-02 14:41:50 -0600

    move from a selection model to a more responsive cell menu model

--- client/js/controllers/listCtrl.js
+++ client/js/controllers/listCtrl.js
@@ -4,16 +4,27 @@ export default (resource) => {
         multiSelect: false,
         modifierKeysToMultiSelect: false,
         enableRowHeaderSelection: false,
-        noUnselect: true
+        noUnselect: true,
     };
+
+    const coreColumns = [{
+        name: 'tools',
+        cellTemplate: '<div><button class="btn btn-sm btn-primary" ng-click="grid.appScope.vm.jumpToEditView(row)">Edit</button><button class="btn btn-sm btn-danger" ng-click="grid.appScope.vm.deleteRow(row)">Delete</button></div>',
+        displayName: '',
+        maxWidth: 100,
+        enableSorting: false,
+        enableFiltering: false,
+        enableHiding: false
+    }];
 
     return class {
         /*@ngInject*/
-        constructor(ApiService, $scope, $state, ModelFieldService) {
+        constructor(ApiService, $scope, $state, ModelFieldService, growl) {
             this.api = ApiService(resource);
             this.$scope = $scope;
             this.$state = $state;
             this.ModelFieldService = ModelFieldService;
+            this.growl = growl;
 
             this.initializeGrid();
         }
@@ -24,7 +35,7 @@ export default (resource) => {
             };
 
             this.grid = angular.extend({
-                columnDefs: this.ModelFieldService.getFieldsForList(resource),
+                columnDefs: this.ModelFieldService.getFieldsForList(resource).concat(coreColumns),
                 data: [],
                 onRegisterApi
             }, coreGridOptions, this.options);
@@ -36,8 +47,15 @@ export default (resource) => {
             this.grid.data = this.api.list();
         }
 
-        jumpToEditView() {
-            this.$state.go('^.edit', {id: this.gridApi.selection.getSelectedRows()[0].id});
+        deleteRow(row) {
+            this.api.remove({id: row.entity.id}).$promise.then(() => {
+                this.growl.success('Delete successful');
+                this.getData();
+            });
+        }
+
+        jumpToEditView(row) {
+            this.$state.go('^.edit', {id: row.entity.id});
         }
     };
 };
